8b7eaf2b05bdb9cd4d34a5fda24a415c8589fd24
Ana <anaarantes@usp.br>
2025-09-07 18:03:52 -0300

    Fix: jogo agora tira um jogador que escolheu parar, além disso para o jogo caso não haja mais moviemntos a serem feitos

--- utils/jogo.py
+++ utils/jogo.py
@@ -4,9 +4,10 @@ from utils.jogadores import Jogador
 class Jogo:
     def __init__(self, jogadores:list):
         self.baralho = Baralho()
-        self.dealer = Jogador('dealer')
+        #self.dealer = Jogador('dealer')
         self.jogadores = jogadores
         for jogador in jogadores:
+            print(f'{jogador.nome.title()} : ')
             valores = self.baralho.tiraragem_inicial()
             jogador.adc_pontos([valores[0], valores[1]])
             
@@ -15,8 +16,19 @@ class Jogo:
             print('Jogador não está no jogo!')
         else:
             jogador.adc_pontos(self.baralho.tirar())
-            self.status = jogador.checar_estado()
+            self.status = jogador.checar_estado(silencio = True)
             return self.status
+
+def criar_jogadores(quantidade_jogadores:int):
+  lista_nomes = []
+  for j in range(1, int(quantidade_jogadores)+1):
+    print(f'Insira o nome do jogador {j}:')
+    nome = input()
+    lista_nomes.append(Jogador(nome)) 
+  #lista_nomes.append(Jogador('Dealer'))
+  print('\n')
+  return lista_nomes
+
 
 if __name__ == '__main__':
     from baralho import Baralho
